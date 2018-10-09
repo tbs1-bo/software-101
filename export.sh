76b285ba2cf722f39818be71c8dd8adb76b2a60b
Marco Bakera <marco@bakera.de>
2018-10-09 12:03:42 +0200

    removing ansi escape sequences

--- export.sh
+++ export.sh
@@ -10,4 +10,8 @@ jupyter nbconvert --to markdown --output $FILEMD $FILE
 # remove magic cell information
 # -i work in-place with no backup files ("")
 #
+echo removing magic lines from $DIR/$FILEMD
 sed -i '' -e "/%%writefile/d; /^    Writing /d; /^    Overwriting/d; /^    Appending/d; s/^! //" $DIR/$FILEMD
+echo removing ansi escape sequences
+# <ESC>  [  0  ;  1  ;  4  m
+sed -i '' -e "s/\x1b\[[0-9]*;*[0-9]*;*[0-9]*m//g" $DIR/$FILEMD
