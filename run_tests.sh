fe0d87f0df229f04ab9f080b8e265b77e9af8168
Marco Bakera <marco@bakera.de>
2020-07-15 14:51:20 +0200

    added todo

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO better use negative list of dirs not to check instead of positive list.
+
 for d in matplotlib persistenz; do
     echo testing $d
     venv/bin/pytest --nbval $d || exit 1
