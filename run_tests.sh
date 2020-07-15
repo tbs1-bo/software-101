c74a448b86002bac804e32cd866c078768baad45
Marco Bakera <marco@bakera.de>
2020-07-15 15:36:48 +0200

    switching to python script.

--- run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# TODO better use negative list of dirs not to check instead of positive list.
-
-for d in matplotlib persistenz; do
-    echo "testing $d"
-    venv/bin/pytest --nbval $d || exit 1
-done
