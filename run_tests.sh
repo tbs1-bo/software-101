4de17c2b13015a0c67eba7614c9cda2f4b9c4581
Marco Bakera <marco@bakera.de>
2020-07-15 14:55:33 +0200

    minor

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@
 # TODO better use negative list of dirs not to check instead of positive list.
 
 for d in matplotlib persistenz; do
-    echo testing $d
+    echo "testing $d"
     venv/bin/pytest --nbval $d || exit 1
 done
