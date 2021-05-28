ac0b5bfc5bc10fd47270eaef7d23f0cefdf10f62
Marco Bakera <marco@bakera.de>
2021-05-28 08:26:03 +0200

    flask: removing stuff

--- flask/process_notebook.sh
+++ flask/process_notebook.sh
@@ -7,6 +7,8 @@ OUT=flask_final.ipynb
 
 echo "converting: $IN -> $OUT"
 
-echo 'removing magic-cell markers: %%'
-grep -v '%%' $IN > $OUT
+echo 'removing magic-cell markers: %% and !'
+grep -v '%%' $IN | sed -e 's/"! /"/' > $OUT
 
+echo "showing changes"
+diff -U 1 $IN $OUT
